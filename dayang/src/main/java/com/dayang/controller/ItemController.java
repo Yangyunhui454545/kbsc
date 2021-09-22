fd472a8503987e81bdd8cdac7b8ddd79270c4d1e
yangyunhui454545 <codingbutter@naver.com>
2021-09-23 00:09:26 +0900

    controller 추가 수정

--- dayang/src/main/java/com/dayang/controller/ItemController.java
+++ dayang/src/main/java/com/dayang/controller/ItemController.java
@@ -31,170 +31,53 @@ public class ItemController {
     public String categoryItem(@PathVariable("list") Long category_id,
                                @RequestParam(value = "sort", defaultValue = "item_id") String sort, Model model,
                                @RequestParam(value="page", defaultValue = "1") Integer pageNum){
-        Category category = itemService.findOneCategory(category_id);
-        List<Category> categories = itemService.getFriendCategory(category);
-
-        List<Category> showCategory = new ArrayList<>();
-        List<Category> tmp;
-        for(Category c : categories){            tmp = itemService.getFriendCategory(c);
-            showCategory.add(c);
-            if(tmp.size()!=0){
-                for(Category cc : tmp){
-                    showCategory.add(cc);
-                }
-            }
-        }
-        if(itemService.getByCategory(category_id).size()!=0){
-            showCategory.add(category);
-        }
-        List<Item> items;
-        items = itemService.findItemByCategory(showCategory, pageNum, sort);
-        long maxNum = itemService.getItemNum(showCategory);
-        model.addAttribute("maxNum", maxNum);
-        model.addAttribute("category", category_id);
-        model.addAttribute("item", items);
-
 
         return "searchItem/category";
     }
 
     @GetMapping("/item/{item_id}")
     public String Item(@PathVariable("item_id")Long id, Model model){
-        Item item = itemService.findOne(id);
-        itemService.updatePopularity(item.getId(), item.getPopularity());
 
-        Item_img item_img = itemService.itemImg(item.getId());
-        int cnt = itemService.reviewCnt(item);
-        double rate = itemService.reviewRate(item);
-
-        if(item.getReviewCnt() !=cnt )itemService.updateReviewCnt(item.getId(), cnt);
-
-        model.addAttribute("item_id", item.getId());
-        model.addAttribute("item_name",item.getName());
-        model.addAttribute("item_img", item_img);
-        model.addAttribute("item_price", item.getItem_price());
-        model.addAttribute("item_discountPrice", item.getDiscount_price());
-        model.addAttribute("item_isTestable", item.getIs_testable());
-        model.addAttribute("review_rate", rate);
-        model.addAttribute("review_count", cnt);
 
         return "searchItem/item";
     }
     @GetMapping("/item/{item_id}/detail")
     public String ItemDetail(@PathVariable("item_id") Long id, Model model){
-        Item item = itemService.findOne(id);
-        List<Product_img> product_img = itemService.productImgs(item);
-        model.addAttribute("item_id", item.getId());
-        model.addAttribute("item_name", item.getName());
-        model.addAttribute("item_isTestable", item.getIs_testable());
-        model.addAttribute("product_img",product_img);
+
         return "searchItem/item-detail";
     }
 
 
     @GetMapping("item/{store_id}/{item_id}/item_option")
     public String ItemOption(@PathVariable("item_id") Long id, @PathVariable("store_id") Long store_id, Model model){
-        Item item = itemService.findOne(id);
-        Store store = storeService.findById(store_id);
-        List<Item_option> item_options = itemService.itemOptionList(item);
 
-        List<StoreQuantity> storeQuantities = new ArrayList<>();
-        for (Item_option item_option : item_options){
-            storeQuantities.add(itemService.storeQuantityList(item_option, store));
-        }
-
-
-        model.addAttribute("item_option", item_options);
-        model.addAttribute("store_Quantity", storeQuantities);
         return "searchItem/item-option";
     }
 
     @GetMapping("/searchVoice")
     public String searchVoicePage(Model model) {
-        List<Item> items = itemService.popularity();
-        model.addAttribute("item", items);
-        return "searchItem/voice-search"; }
+
+        return "searchItem/voice-search";
+    }
 
     @GetMapping("/searchVoice/{keyword}")
     public String searchVoice(@PathVariable("keyword")String keyword, Model model, @RequestParam(value= "sort", defaultValue = "reviewCnt") String sort, @RequestParam(value="page", defaultValue = "1") Integer pageNum){
 
 
 
-        Page<Item> items = itemService.findByItemName(keyword, pageNum, "reviewCnt");
-
-        if(items.getTotalElements()==0){
-            model.addAttribute("count", items.getTotalElements());
-            return "searchItem/search-result";
-        }
-
-        model.addAttribute("keyword", keyword);
-        model.addAttribute("item", items);
-        model.addAttribute("count", items.getTotalElements());
-        model.addAttribute("maxNum", items.getTotalPages());
         return "searchItem/search-result";
     }
 
-    @GetMapping("/searchItem/{keyword}")
-    public String search(@PathVariable("keyword")String keyword, Model model, @RequestParam(value="sort", defaultValue = "id")String sort, @RequestParam(value="page", defaultValue = "1") Integer pageNum){
-        if(itemService.findByItemName(keyword, pageNum, sort).getTotalElements()>0) {
-            Page<Item> items = itemService.findByItemName(keyword, pageNum, sort);
-            List<Item_img> item_imgs = new ArrayList<>();
-            for(Item item : items){
-                item_imgs.add(itemService.itemImg(item.getId()));
-            }
-            model.addAttribute("item",itemService.findByBrandName(keyword));
-            model.addAttribute("item_img", item_imgs);
-            model.addAttribute("item_count", items.getTotalElements());
-        }
-        if(itemService.findByBrandName(keyword)!=null)model.addAttribute("brand", itemService.findByBrandName(keyword));
-        if(itemService.getCategoryByName(keyword)!=null)model.addAttribute("category", itemService.getCategoryByName(keyword));
-
-        return "searchItem/search-result";
-    }
 
     @GetMapping("/review/{item_id}")
     public String review(@PathVariable("item_id")Long id, Model model){
-        Item item = itemService.findOne(id);
-        List<Review> reviews = itemService.findReviewByItem(item);
-        List<Review_img> review_imgs = new ArrayList<>();
-        for(Review review : reviews) review_imgs.add(itemService.getReviewImg(review));
-        model.addAttribute("item", item);
-        model.addAttribute("review", reviews);
-        model.addAttribute("review_img", review_imgs);
+
         return "searchItem/review";
     }
 
-
     @GetMapping("/todayGoods")
     public String bestItem(Model model){
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(new Date());
-        Date time = calendar.getTime();
-
-        String date = "2021-";
-        int month = time.getMonth();
-        date +=Integer.toString(month);
-        date +="-01";
-        List<Item> BestItems = itemService.newItem(date);
-        List<Item_img> BestItemImg = new ArrayList<>();
-        for(Item item : BestItems){
-            BestItemImg.add(itemService.itemImg(item.getId()));
-        }
-        model.addAttribute("BestItemImg", BestItemImg);
-        model.addAttribute("BestItems", BestItems);
-
-        List<Item> mdsPickItem = itemService.recommendItem();
-        List<Item_img> mdsPickItemImg = new ArrayList<>();
-        for(Item item : mdsPickItem){
-            mdsPickItemImg.add(itemService.itemImg(item.getId()));
-        }
-
-        model.addAttribute("MdItemImg", mdsPickItemImg);
-        model.addAttribute("MdItems", mdsPickItem);
-
         return "searchItem/today-goods";
 
     }
-
-
 }
--- dayang/src/main/java/com/dayang/controller/StoreController.java
+++ dayang/src/main/java/com/dayang/controller/StoreController.java
@@ -17,25 +17,13 @@ import java.util.*;
 @RequiredArgsConstructor
 public class StoreController {
     private final StoreService storeService;
-    private final ItemService itemService;
-    private final OrderService orderService;
 
     @GetMapping("/stores")
     public String stores(Model model){
-        List<Store> stores = storeService.findAllStore();
-        List<Position> positions = storeService.findAllPosition();
-        model.addAttribute("stores", stores);
-        model.addAttribute("positions", positions);
+
         return "store/stores";
     }
 
-    @GetMapping("/soldout/{store_id}/{item_option_id}")
-    public String soldout(@PathVariable("store_id")Long id, @PathVariable("item_option_id")Long option_id, Model model){
-
-
-        return "store/near-store";
-
-    }
 
     @GetMapping("/storeDistance/{store_id}")
     public String distance(@PathVariable("store_id")Long id, Model model){
@@ -43,35 +31,5 @@ public class StoreController {
 
         return "store/store-content";
     }
-    @GetMapping("/admin/{store_id}")
-    public String admin(@PathVariable("store_id") Long id, @RequestParam("code") String pw,  Model model){
-        Store store = storeService.findById(id);
-        if(!store.getCode().equals(pw)){
-            return "login/fail";
-        }
-
-        int todaySale = orderService.todaySales(store);
-        int weekSale = orderService.thisWeekSales(store);
-        List<StoreQuantity> storeQuantities = itemService.soldOutOptions(store);
-        List<Item> item = new ArrayList<>();
-        List<Item_option>item_options = new ArrayList<>();
-        List<Item_img> item_imgs = new ArrayList<>();
-        for(StoreQuantity storeQuantity : storeQuantities){
-            item.add(itemService.findOne(storeQuantity.getItem().getId()));
-            item_options.add(itemService.findItemOptionById(storeQuantity.getItemOption().getId()));
-            item_imgs.add(itemService.itemImg(storeQuantity.getItem().getId()));
-        }
-
-
-        model.addAttribute("todaySale", todaySale);
-        model.addAttribute("weekSale", weekSale);
-        model.addAttribute("item", item);
-        model.addAttribute("item_options", item_options);
-        model.addAttribute("item_img", item_imgs);
-
-        return "login/admin";
-    }
-
-
 
 }
